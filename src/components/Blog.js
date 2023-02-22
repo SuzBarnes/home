8209566ce5c98a199400dde075200ec5bf9470ad
Suzannah Barnes <smgarland@hotmail.co.uk>
2023-02-22 17:05:40 +0000

    blogcard 2

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "../styling/blog.scss";
 import BlogCard1 from "./BlogCard1";
+import BlogCard2 from "./BlogCard2";
 
 const Blog = () => {
   return (
     <div>
       <h3>Follow my journey...</h3>
-      <BlogCard1 />
+      <div className="blog">
+        <BlogCard2 />
+      </div>
+      <div className="blog">
+        <BlogCard1 />
+      </div>
     </div>
   );
 };
